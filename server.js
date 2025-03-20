b1a86b43b7622bfb350e7421e8b149e08f7cd697
muthimenijyoshna7095 <muthimenijyoshna7095@gmail.com>
2025-03-20 19:00:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./db");
+const routes = require("./routes"); // Import routes
+const cors = require("cors");
+require("dotenv").config();
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to Database
+connectDB();
+
+// Use Routes (Consistent Path)
+app.use("/api", routes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
